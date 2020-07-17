a62dd0f5ceaeab7abad666f8d925e65291ecaa71
Diego Lucas <diego.lucasilva@gmail.com>
2020-07-17 20:10:51 -0300

    jedis demo example

---
+++ redis-client/src/main/java/com/dls/redisclient/RedisClientApplication.java
@@ -0,0 +1,13 @@
+package com.dls.redisclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisClientApplication.class, args);
+	}
+
+}
