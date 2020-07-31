7b5d8a62638f4ab48011332d1a422f65c5e60e3a
Diego Lucas <diego.lucasilva@gmail.com>
2020-07-31 14:55:37 -0300

    more index

--- redis-client/src/main/java/br/com/dls/redisclient/domain/Customer.java
+++ redis-client/src/main/java/br/com/dls/redisclient/domain/Customer.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Reference;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.util.List;
 
@@ -13,11 +14,10 @@ public class Customer {
 
     @Id
     private String id;
+    @Indexed
     private String name;
-
     @Reference
     private List<Account> accounts;
-
     private Address address;
 
 }
